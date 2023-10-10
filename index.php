02353a675937d1d17191b1b7a40ceea799c75c7b
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-10 17:00:20 +0200

    add style

--- index.php
+++ index.php
@@ -53,12 +53,12 @@ $hotels = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-info">
 
     <div class="container mt-5">
         <div class="row">
 
-            <table class="table table-bordered">
+            <table class="table table-dark table-bordered">
                 <thead>
                     <tr>
                         <th>
@@ -78,7 +78,7 @@ $hotels = [
                         </th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="table-group-divider">
                     <?php foreach ($hotels as $hotel) : ?>
 
 
